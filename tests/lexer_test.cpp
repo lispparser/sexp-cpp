24f38e788f7f29c814f98f2834ed051885b8aa7f
Ingo Ruhnke <grumbel@gmail.com>
2015-10-11 19:29:49 +0200

    Added test case that fails for long tokens

---
+++ tests/lexer_test.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+
+#include <sstream>
+
+#include "lisp/lexer.hpp"
+
+TEST(LexerTest, simple_tokens)
+{
+  std::istringstream out("(foo bar)");
+  lisp::Lexer lexer(out);
+  ASSERT_EQ(lisp::Lexer::TOKEN_OPEN_PAREN, lexer.getNextToken());
+  ASSERT_EQ(lisp::Lexer::TOKEN_SYMBOL, lexer.getNextToken());
+  ASSERT_STREQ("foo", lexer.getString());
+  ASSERT_EQ(lisp::Lexer::TOKEN_SYMBOL, lexer.getNextToken());
+  ASSERT_STREQ("bar", lexer.getString());
+  ASSERT_EQ(lisp::Lexer::TOKEN_CLOSE_PAREN, lexer.getNextToken());
+  ASSERT_EQ(lisp::Lexer::TOKEN_EOF, lexer.getNextToken());
+}
+
+TEST(LexerTest, long_tokens)
+{
+  std::string long_token(32768, 'X');
+  std::istringstream out("(" + long_token + ")");
+  lisp::Lexer lexer(out);
+  ASSERT_EQ(lisp::Lexer::TOKEN_OPEN_PAREN, lexer.getNextToken());
+  ASSERT_EQ(lisp::Lexer::TOKEN_SYMBOL, lexer.getNextToken());
+  ASSERT_EQ(long_token, lexer.getString());
+  ASSERT_EQ(lisp::Lexer::TOKEN_CLOSE_PAREN, lexer.getNextToken());
+  ASSERT_EQ(lisp::Lexer::TOKEN_EOF, lexer.getNextToken());
+}
+
+/* EOF */
