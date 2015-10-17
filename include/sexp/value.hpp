6075e5cbcb7398e84e2e57edba62f5dd9da66753
Ingo Ruhnke <grumbel@gmail.com>
2015-10-17 10:45:40 +0200

    Store atoms directly in Value, instead of ValueImpl, only strings and cons are allocated
    This increases the size of Value 16 bytes, instead of 8 byte, but seems to be about ~35% faster:
    
    $ build/benchmarks_old/parser
    Run on (8 X 1400 MHz CPU s)
    2015-10-17 10:41:39
    ***WARNING*** CPU scaling is enabled, the benchmark real time measurements may be noisy and will incure extra overhead.
    Benchmark    Time(ns)    CPU(ns) Iterations
    -------------------------------------------
    BM_parser     8721429    8650000         80
    
    $ build/benchmarks_old/parser
    Run on (8 X 1400 MHz CPU s)
    2015-10-17 10:41:41
    ***WARNING*** CPU scaling is enabled, the benchmark real time measurements may be noisy and will incure extra overhead.
    Benchmark    Time(ns)    CPU(ns) Iterations
    -------------------------------------------
    BM_parser     6270380    6256881        109

--- include/sexp/value.hpp
+++ include/sexp/value.hpp
@@ -23,8 +23,6 @@
 
 namespace sexp {
 
-class ValueImpl;
-
 class Value
 {
 public:
@@ -40,44 +38,123 @@ public:
   };
 
 private:
-  std::unique_ptr<ValueImpl> m_impl;
+  struct Cons;
+
+  Value::Type m_type;
+  union
+  {
+    bool m_bool;
+    int m_int;
+    float m_float;
+
+    std::string* m_string;
+    Cons* m_cons;
+  };
 
 public:
   /** Returns a reference to a nil value for use in functions that
       return a reference and need to return a nil value */
   static Value const& nil_ref() { static Value const s_nil; return s_nil; }
   static Value nil() { return Value(); }
-  static Value boolean(bool v) { return Value(std::make_unique<ValueImpl>(v)); }
-  static Value integer(int v) { return Value(std::make_unique<ValueImpl>(v)); }
-  static Value real(float v) { return Value(std::make_unique<ValueImpl>(v)); }
-  static Value string(std::string const& v) { return Value(std::make_unique<ValueImpl>(TYPE_STRING, v)); }
-  static Value symbol(std::string const& v) { return Value(std::make_unique<ValueImpl>(TYPE_SYMBOL, v)); }
-  static Value cons(Value&& car, Value&& cdr) { return Value(std::make_unique<ValueImpl>(std::move(car), std::move(cdr))); }
-  static Value cons() { return Value(std::make_unique<ValueImpl>(Value::nil(), Value::nil())); }
+  static Value boolean(bool v) { return Value(v); }
+  static Value integer(int v) { return Value(v); }
+  static Value real(float v) { return Value(v); }
+  static Value string(std::string const& v) { return Value(TYPE_STRING, v); }
+  static Value symbol(std::string const& v) { return Value(TYPE_SYMBOL, v); }
+  static Value cons(Value&& car, Value&& cdr) { return Value(std::move(car), std::move(cdr)); }
+  static Value cons() { return Value(Value::nil(), Value::nil()); }
+
+private:
+  Value(bool value) : m_type(TYPE_BOOLEAN), m_bool(value) {}
+  Value(int value) : m_type(TYPE_INTEGER), m_int(value) {}
+  Value(float value) : m_type(TYPE_REAL), m_float(value) {}
+  Value(Type type, std::string const& value) :
+    m_type(type),
+    m_string(new std::string(value))
+  {}
+  Value(Value&& car, Value&& cdr);
+
+  void destroy();
 
 public:
-  explicit Value(Value const& other) :
-    m_impl(other.is_nil() ? nullptr : std::make_unique<ValueImpl>(*other.m_impl))
-  {}
+  explicit Value(Value const& other);
 
   Value(Value&& other) :
-    m_impl(std::move(other.m_impl))
-  {}
+    m_type(other.m_type)
+  {
+    switch(m_type)
+    {
+      case TYPE_NIL:
+        break;
+
+      case TYPE_BOOLEAN:
+        m_bool = other.m_bool;
+        break;
+
+      case TYPE_INTEGER:
+        m_int = other.m_int;
+        break;
+
+      case TYPE_REAL:
+        m_float = other.m_float;
+        break;
+
+      case TYPE_STRING:
+      case TYPE_SYMBOL:
+        m_string = other.m_string;
+        break;
+
+      case TYPE_CONS:
+        m_cons = other.m_cons;
+        break;
+    }
+
+    other.m_type = TYPE_NIL;
+  }
 
   Value() :
-    m_impl()
-  {}
-
-  Value(std::unique_ptr<ValueImpl> impl) :
-    m_impl(std::move(impl))
+    m_type(TYPE_NIL)
   {}
 
   ~Value()
-  {}
+  {
+    destroy();
+  }
 
   Value& operator=(Value&& other)
   {
-    m_impl = std::move(other.m_impl);
+    destroy();
+
+    m_type = other.m_type;
+    other.m_type = TYPE_NIL;
+
+    switch(m_type)
+    {
+      case TYPE_NIL:
+        break;
+
+      case TYPE_BOOLEAN:
+        m_bool = other.m_bool;
+        break;
+
+      case TYPE_INTEGER:
+        m_int = other.m_int;
+        break;
+
+      case TYPE_REAL:
+        m_float = other.m_float;
+        break;
+
+      case TYPE_STRING:
+      case TYPE_SYMBOL:
+        m_string = other.m_string;
+        break;
+
+      case TYPE_CONS:
+        m_cons = other.m_cons;
+        break;
+    }
+
     return *this;
   }
 
@@ -85,7 +162,7 @@ public:
 
   explicit operator bool() const
   {
-    return static_cast<bool>(m_impl);
+    return m_type != TYPE_NIL;
   }
 
   bool is_nil() const;
@@ -115,146 +192,99 @@ public:
   std::string str() const;
 };
 
-class ValueImpl
+struct Value::Cons
 {
-public:
-  Value::Type m_type;
-  union
-  {
-    struct
-    {
-      Value car;
-      Value cdr;
-    } m_cons;
-
-    std::string* m_string;
-    bool m_bool;
-    int m_int;
-    float m_float;
-  };
-
-  ValueImpl(ValueImpl const& other) :
-    m_type(other.m_type)
-  {
-    switch(m_type)
-    {
-      case Value::TYPE_NIL:
-        assert(false && "ValueImpl can't be TYPE_NIL");
-        break;
-
-      case Value::TYPE_BOOLEAN:
-        m_bool = other.m_bool;
-        break;
-
-      case Value::TYPE_INTEGER:
-        m_int = other.m_int;
-        break;
-
-      case Value::TYPE_REAL:
-        m_float = other.m_float;
-        break;
-
-      case Value::TYPE_CONS:
-        new (&m_cons.car) Value(other.m_cons.car);
-        new (&m_cons.cdr) Value(other.m_cons.cdr);
-        break;
-
-      case Value::TYPE_SYMBOL:
-      case Value::TYPE_STRING:
-        m_string = new std::string(*other.m_string);
-        break;
-    }
-  }
-
-  ValueImpl(Value&& car, Value&& cdr) :
-    m_type(Value::TYPE_CONS),
-    m_cons{std::move(car), std::move(cdr)}
-  {}
-
-  ValueImpl(Value::Type type, std::string const& text) :
-    m_type(type),
-    m_string(new std::string(text))
-  {}
-
-  ValueImpl(bool v) :
-    m_type(Value::TYPE_BOOLEAN),
-    m_bool(v)
-  {}
-
-  ValueImpl(int v) :
-    m_type(Value::TYPE_INTEGER),
-    m_int(v)
-  {}
-
-  ValueImpl(float v) :
-    m_type(Value::TYPE_REAL),
-    m_float(v)
-  {}
-
-  ~ValueImpl()
-  {
-    switch(m_type)
-    {
-      case Value::TYPE_STRING:
-      case Value::TYPE_SYMBOL:
-        delete m_string;
-        break;
-
-      case Value::TYPE_CONS:
-        m_cons.car.~Value();
-        m_cons.cdr.~Value();
-        break;
-
-      default:
-        // atoms don't need deletion
-        break;
-    }
-  }
-
-  bool operator==(ValueImpl const& rhs) const
-  {
-    if (m_type != rhs.m_type)
-    {
-      return false;
-    }
-    else
-    {
-      switch(m_type)
-      {
-        case Value::TYPE_BOOLEAN:
-          return m_bool == rhs.m_bool;
-
-        case Value::TYPE_INTEGER:
-          return m_int == rhs.m_int;
-
-        case Value::TYPE_REAL:
-          return m_float == rhs.m_float;
-
-        case Value::TYPE_STRING:
-        case Value::TYPE_SYMBOL:
-
-        case Value::TYPE_CONS:
-          return (m_cons.car == m_cons.car &&
-                  m_cons.cdr == m_cons.cdr);
-
-        default:
-          assert(false && "should never be reached");
-          return false;
-      }
-    }
-  }
+  Value car;
+  Value cdr;
 };
 
-inline bool
-Value::operator==(Value const& other) const
+inline
+Value::Value(Value&& car, Value&& cdr) :
+  m_type(TYPE_CONS),
+  m_cons(new Cons{std::move(car), std::move(cdr)})
+{}
+
+inline void
+Value::destroy()
 {
-  if (m_impl == other.m_impl)
+  switch(m_type)
   {
-    return true;
+    case Value::TYPE_STRING:
+    case Value::TYPE_SYMBOL:
+      delete m_string;
+      break;
+
+    case Value::TYPE_CONS:
+      delete m_cons;
+      break;
+
+    default:
+      // atoms don't need deletion
+      break;
   }
-  else if (m_impl)
+}
+
+inline
+Value::Value(Value const& other) :
+  m_type(other.m_type)
+{
+  switch(m_type)
   {
-    return *m_impl == *other.m_impl;
+    case TYPE_NIL:
+      break;
+
+    case TYPE_BOOLEAN:
+      m_bool = other.m_bool;
+      break;
+
+    case TYPE_INTEGER:
+      m_int = other.m_int;
+      break;
+
+    case TYPE_REAL:
+      m_float = other.m_float;
+      break;
+
+    case TYPE_STRING:
+    case TYPE_SYMBOL:
+      m_string = new std::string(*other.m_string);
+      break;
+
+    case TYPE_CONS:
+      m_cons = new Cons(*other.m_cons);
+      break;
+  }
+}
+
+inline bool
+Value::operator==(Value const& rhs) const
+{
+  if (m_type == rhs.m_type)
+  {
+    switch(m_type)
+    {
+      case TYPE_NIL:
+        return true;
+
+      case Value::TYPE_BOOLEAN:
+        return m_bool == rhs.m_bool;
+
+      case Value::TYPE_INTEGER:
+        return m_int == rhs.m_int;
+
+      case Value::TYPE_REAL:
+        return m_float == rhs.m_float;
+
+      case Value::TYPE_STRING:
+      case Value::TYPE_SYMBOL:
+        return *m_string == *rhs.m_string;
+
+      case Value::TYPE_CONS:
+        return (m_cons->car == rhs.m_cons->car &&
+                m_cons->cdr == rhs.m_cons->cdr);
+    }
+    assert(false && "should never be reached");
+    return false;
   }
   else
   {
@@ -265,112 +295,112 @@ Value::operator==(Value const& other) const
 inline Value::Type
 Value::get_type() const
 {
-  return (m_impl ? m_impl->m_type : Value::TYPE_NIL);
+  return m_type;
 }
 
 inline bool
 Value::is_nil() const
 {
-  return m_impl == nullptr;
+  return m_type == TYPE_NIL;
 }
 
 inline bool
 Value::is_cons() const
 {
-  return m_impl && m_impl->m_type == Value::TYPE_CONS;
+  return m_type == TYPE_CONS;
 }
 
 inline bool
 Value::is_symbol() const
 {
-  return m_impl && m_impl->m_type == Value::TYPE_SYMBOL;
+  return m_type == TYPE_SYMBOL;
 }
 
 inline bool
 Value::is_string() const
 {
-  return m_impl && m_impl->m_type == Value::TYPE_STRING;
+  return m_type == TYPE_STRING;
 }
 
 inline bool
 Value::is_integer() const
 {
-  return m_impl && m_impl->m_type == Value::TYPE_INTEGER;
+  return m_type == TYPE_INTEGER;
 }
 
 inline bool
 Value::is_real() const
 {
-  return m_impl && (m_impl->m_type == Value::TYPE_REAL ||
-                    m_impl->m_type == Value::TYPE_INTEGER);
+  return (m_type == TYPE_REAL ||
+          m_type == TYPE_INTEGER);
 }
 
 inline Value const&
 Value::get_car() const
 {
-  assert(m_impl->m_type == Value::TYPE_CONS);
-  return m_impl->m_cons.car;
+  assert(m_type == TYPE_CONS);
+  return m_cons->car;
 }
 
 inline Value const&
 Value::get_cdr() const
 {
-  assert(m_impl->m_type == Value::TYPE_CONS);
-  return m_impl->m_cons.cdr;
+  assert(m_type == TYPE_CONS);
+  return m_cons->cdr;
 }
 
 inline Value&
 Value::get_car()
 {
-  assert(m_impl->m_type == Value::TYPE_CONS);
-  return m_impl->m_cons.car;
+  assert(m_type == TYPE_CONS);
+  return m_cons->car;
 }
 
 inline Value&
 Value::get_cdr()
 {
-  assert(m_impl->m_type == Value::TYPE_CONS);
-  return m_impl->m_cons.cdr;
+  assert(m_type == TYPE_CONS);
+  return m_cons->cdr;
 }
 
 inline void
 Value::set_car(Value&& sexpr)
 {
-  assert(m_impl->m_type == Value::TYPE_CONS);
-  m_impl->m_cons.car = std::move(sexpr);
+  assert(m_type == TYPE_CONS);
+  m_cons->car = std::move(sexpr);
 }
 
 inline void
 Value::set_cdr(Value&& sexpr)
 {
-  assert(m_impl->m_type == Value::TYPE_CONS);
-  m_impl->m_cons.cdr = std::move(sexpr);
+  assert(m_type == TYPE_CONS);
+  m_cons->cdr = std::move(sexpr);
 }
 
 inline bool
 Value::as_bool() const
 {
-  assert(m_impl->m_type == Value::TYPE_BOOLEAN);
-  return m_impl->m_bool;
+  assert(m_type == TYPE_BOOLEAN);
+  return m_bool;
 }
 
 inline int
 Value::as_int() const
 {
-  assert(m_impl->m_type == Value::TYPE_INTEGER);
-  return m_impl->m_int;
+  assert(m_type == TYPE_INTEGER);
+  return m_int;
 }
 
 inline float
 Value::as_float() const
 {
-  if (m_impl->m_type == Value::TYPE_REAL)
+  if (m_type == TYPE_REAL)
   {
-    return m_impl->m_float;
+    return m_float;
   }
-  else if (m_impl->m_type == Value::TYPE_INTEGER)
+  else if (m_type == TYPE_INTEGER)
   {
-    return static_cast<float>(m_impl->m_int);
+    return static_cast<float>(m_int);
   }
   else
   {
@@ -382,9 +412,9 @@ Value::as_float() const
 inline std::string const&
 Value::as_string() const
 {
-  assert(m_impl->m_type == Value::TYPE_SYMBOL ||
-         m_impl->m_type == Value::TYPE_STRING);
-  return *m_impl->m_string;
+  assert(m_type == TYPE_SYMBOL ||
+         m_type == TYPE_STRING);
+  return *m_string;
 }
 
 } // namespace sexp
