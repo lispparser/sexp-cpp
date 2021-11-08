ec1c33d05415ef137dd6c79a342aae9c8d6bdd63
Ingo Ruhnke <grumbel@gmail.com>
2021-11-08 10:53:28 +0100

    Fix tinycmmc URL in Nix flake

--- flake.nix
+++ flake.nix
@@ -1,23 +1,22 @@
 {
+  description = "A S-Expression Parser for C++";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    tinycmmc.url = "git+file:///home/ingo/projects/lispparser/sexp-cpp/external/cmake-modules/";
+    tinycmmc.url = "gitlab:grumbel/cmake-modules";
   };
-
-  description = "sexp-cpp flake";
 
   outputs = { self, nix, nixpkgs, flake-utils, tinycmmc }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        inherit (nixpkgs) lib;
       in rec {
         packages = flake-utils.lib.flattenTree {
           sexp-cpp = pkgs.stdenv.mkDerivation {
             pname = "sexp-cpp";
-            version = "0.0.0";
-            src = lib.cleanSource ./.;
+            version = "0.1.0";
+            src = nixpkgs.lib.cleanSource ./.;
             nativeBuildInputs = [ pkgs.cmake pkgs.ninja pkgs.gcc tinycmmc.defaultPackage.${system} ];
            };
         };
