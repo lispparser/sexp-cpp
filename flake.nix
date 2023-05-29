3883208051bf2615c6df01140436843de726f4d0
Ingo Ruhnke <grumbel@gmail.com>
2023-05-29 13:51:38 +0200

    Update flake.nix to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A S-Expression Parser for C++";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
@@ -14,8 +14,8 @@
       {
         packages = rec {
           default = sexp-cpp;
+
           sexp-cpp = pkgs.callPackage ./sexpcpp.nix {
-            stdenv = pkgs.gcc12Stdenv;
             tinycmmc = tinycmmc.packages.${pkgs.system}.default;
           };
         };
