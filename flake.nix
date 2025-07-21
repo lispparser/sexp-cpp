677b6f3ecd54e92339d33062084b081ebb9f14a6
Ingo Ruhnke <grumbel@gmail.com>
2025-07-21 19:52:56 +0200

    Update to nixos-25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A S-Expression Parser for C++";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
